30afdba130f5664ebb443b8579e16bc92a3b0163
ShambhaviC56 <Shambhavi.Chandel@jadeglobal.com>
2026-04-22 14:05:39 +0530

    Add Playwright JavaScript automation script: undefined

---
+++ automation-scripts/Playwright/JavaScript/locators/locators.js
@@ -0,0 +1,6 @@
+module.exports = {
+  usernameField: '#username',
+  passwordField: '#password',
+  searchPlaceholderInput: 'input[placeholder="Search..."]',
+  accountTitleLink: 'a[title="Gamma Services - TCV Test"]'
+};
